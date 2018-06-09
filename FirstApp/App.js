fdd575d0587b325b96fcba31d9990e57a22cb980
Dang <tzero1898@gmail.com>
2018-06-09 08:23:55 +0700

    Changed

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -1,58 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+import React,{Component} from 'react';
+import {View, StyleSheet,Text} from 'react-native';
+class FirstApp extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    return (<Text>Hello {this.props.name}</Text>);
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class LotsOfApp extends Component {
+  render() {
+    return (<View style={{
+        alignItems: 'center'
+      }}>
+      <FirstApp name='Vu'></FirstApp>
+      <FirstApp name='Vu'></FirstApp>
+      <FirstApp name='Hai'></FirstApp>
+    </View>);
+  }
+}
